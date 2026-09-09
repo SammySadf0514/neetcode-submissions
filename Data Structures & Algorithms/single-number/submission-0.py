ca09210f9ae859947b1fd7bc097bb52db987d320
SammySadf0514 <127101481+SammySadf0514@users.noreply.github.com>
2026-09-09 20:27:16 +0530

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        # xor: a ^ a = 0
+
+        res = 0
+
+        for num in nums:
+            res ^= num
+
+        return res
